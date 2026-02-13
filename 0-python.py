b2220bd04c01acacb73e987ce36bad99a5cf643d
Radar77 <Samjpoyner@icloud.com>
2026-02-12 23:36:55 -0500

    Add files via upload

---
+++ 0-python.py
@@ -0,0 +1,34 @@
+'''
+1.0: Setting up Python
+
+Python is pretty easy to set up, and only requires a few simple steps.
+
+On Windows:
+	Go to https://www.python.org/downloads/ and download the installer for Windows
+	-- OR --
+	Open the Windows app store and install Python from there
+
+On Linux:
+	Check your package manager for "python3"
+
+On Mac:
+	Same as Windows (if your Mac doesn't come with it preinstalled), just download the .dmg instead of the .exe
+
+
+You're also going to want something called "pip", which is a library/package installer for Python.
+I don't remember if it comes with Python or not, so here's how to check:
+
+1. Open the command console
+2. type "pip --version", and if that fails, try "pip3 --version".
+   If pip3 works, substitute every time I mention pip with pip3.
+
+Note: the above is only for Windows and Mac. If you're on Linux, get pipx and substitute pip with pipx.
+
+
+Python files always end in the extension ".py". To execute them, run either "python myFile.py" or "python3 myFile.py"
+
+To enter a REPL (basically a terminal/console), just type "python" or "python3"
+
+
+That's all for set up!
+'''
